2771acc4a7f847709028cb85791681b6e31da3da
Angel Martinez <a.angel.martinez@gmail.com>
2012-02-05 20:25:56 -0500

    cleaned up the instructions a bit.

--- database/database.py
+++ database/database.py
@@ -3,7 +3,6 @@
 # WHEN YOU ARE DONE CONFIGURING THE FILE, RUN IT!# 
 
 
-
 import sqlite3
 
 dbfile = sqlite3.connect('') 
@@ -16,12 +15,12 @@ def table():
 	dbcurs.execute('''CREATE TABLE "table name"
 	(id INTEGER PRIMARY KEY, name TEXT, link TEXT)''')
 
-# This execution is creating the table in the database you just made. It's also creating placeholders for the input values.
+# This execution is creating the table in the database you just made. Change the "table name" to the name you want to use (remove quotes). You can edit this however you like: adding/deleting columns.
 
-# "table name" = when you're ready to pick a table name, enter it! (remove quotes)
-# 'id' = each entry will be given an id number. This is to keep track of all of the inputs, which auto-increments. You should leave this one alone!
-# 'name' = this is meant for multiple people who are sharing, like on a team. 
-# 'link' = this is where you would store your links. 
-# You can edit this however you like: adding/deleting columns.
-
-table()
+def main():
+	
+	table()
+	
+	dbfile.commit()
+	
+main()
