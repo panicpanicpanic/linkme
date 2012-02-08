a6b15b575bf5b6a389ebfce6d0aeb878d0f5b962
Angel Martinez <a.angel.martinez@gmail.com>
2012-02-07 23:01:54 -0500

    final push

--- linkme/menu.py
+++ linkme/menu.py
@@ -3,64 +3,47 @@
 
 import sqlite3
 
-dbfile = sqlite3.connect('')
+dbfile = sqlite3.connect('') 
 
 dbcurs = dbfile.cursor()
 
+def table():
+	table = dbcurs.execute('''CREATE TABLE "table name"
+	(id INTEGER PRIMARY KEY, name TEXT, link TEXT)''')
 
 def main():
+	table()
 	menu()
-	insert()
 	insertdb()
-	read()
 	getdb()
-	
 	
 def menu():
 	print('Welcome to LinkMe!')
 	print('Here are your options: ')
-	print('\n')
 	option = raw_input('Do you want to put stuff into the database? (enter i or I) OR read what from it? (enter r or R)?: ')
-
-
-def insert(option):
-	if option == 'i' or 'I':
+	if option == 'i':
 		insertdb()
-	else:
-		print('Confused? Here is the menu again:')
-		menu()	
-		
-def insertdb():
-		name = raw_input('Enter your name or initials: ')
-		link = raw_input('Enter the link you want to store in the database: ')
-		queryCurs.execute('''INSERT INTO "table name" (name, link)		VALUES (?,?)''',(name, link))
-		dbfile.commit()
-		dbfile.close()
-		menu()
-
-
-def read(option):
-	if option == 'r' or 'R':
+	elif option == 'r':
 		getdb()
-	else:
-		print('Confused? Here is the menu again:')
-		menu()
+
+def insertdb():
+	name = raw_input('Enter your name or initials: ')
+	link = raw_input('Enter the link you want to store in the database: ')
+	dbcurs.execute('''INSERT INTO "table name" (name, link) VALUES (?,?)''',(name, link))
+	dbfile.commit()
+	menu()
+
 
 def getdb():
-		dbcurs.execute('SELECT * FROM "table name" ORDER BY id DESC, name LIMIT 10')
-		labels = ['ID', 'name', 'link']
-		t=0
-			
-		for i in dbcurs:
-			print '\n'
-			for j in i:
-				print lables[t]
-				print j
-				if t < 10: t+=1
-				else: t = 0 
-		dbfile.commit()
-		dbfile.close()
-		menu()
+	dbcurs.execute('SELECT * FROM "table name" ORDER BY id ASC, name LIMIT 25')
 
-menu()
-	
+	for i in dbcurs:
+		print '\n'
+		for j in i:
+			print j
+				
+	dbfile.commit()
+	menu()
+
+main()
+
